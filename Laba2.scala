a24840141b62d8fcacba0c410b16c8c4c40b46f1
ilasha <whyalwaysilash@gmail.com>
2016-11-08 18:27:15 +0200

    Лаба 2 (обновленная) + Лаба 3. О'кей, Google, кто-то проверяет мои лабы?)

--- Laba2.scala
+++ Laba2.scala
@@ -1,22 +1,16 @@
 package Laba2
+import scala.collection.immutable.List
 
 /**
   * Created by Ilasha on 07.11.2016.
   */
 
 object Laba2 {
-  def flatMap(x: List[List[Double]], operation: String, m:Double): List[Double] = {
+  def flatMap(x: List[List[Double]], f: Double => Double): List[Double] = {
     var list = List[Double]()
     for(a <- x) {
       for(s <- a) {
-        var f = s
-        operation match {
-          case "*" => f = s * m
-          case "/" => f = s / m
-          case "-" => f = s - m
-          case "+" => f = s + m
-        }
-        list ::= f
+        list ::= f(s)
       }
     }
     list = list.reverse
@@ -24,11 +18,8 @@ object Laba2 {
   }
 }
 
-
-
 object Test extends App {
   var num = List(List(1.0,2.0), List(3.0,4.0))
-  println(num)
-  var flat = Laba2.flatMap(num, "+", 6)
+  var flat = Laba2.flatMap(num, _ * 2)
   println(flat)
 }
---
+++ Laba3.scala
@@ -0,0 +1,110 @@
+/**
+  * Created by Ilasha on 08.11.2016.
+  */
+object Lab3 extends App{
+  val R = FunSets
+  val t1 = Set(1,2,3,4)
+  val t2 = Set(3,6,15,8,10)
+  R.printSet(R.union(R.singletonSet(1), R.singletonSet(3)))
+  R.printSet(R.diff(t2, t1))
+  println(R.forall(t2, _ > 1))
+  R.printSet(R.map(t2, _ + 2))
+}
+
+object FunSets {
+  /**
+    * We represent a set by its characteristic function, i.e.
+    * its `contains` predicate.
+    */
+  type Set = Int => Boolean
+
+  /**
+    * Indicates whether a set contains a given element.
+    */
+  def contains(s: Set, elem: Int): Boolean = s(elem)
+
+  /**
+    * Returns the set of the one given element.
+    */
+  def singletonSet(elem: Int): Set = {
+    Set(elem)
+  }
+
+  /**
+    * Returns the union of the two given sets,
+    * the sets of all elements that are in either `s` or `t`.
+    */
+  def union(s: Set, t: Set): Set = i => s(i) || t(i)
+
+  /**
+    * Returns the intersection of the two given sets,
+    * the set of all elements that are both in `s` and `t`.
+    */
+  def intersect(s: Set, t: Set): Set = i => s(i) && t(i)
+
+  /**
+    * Returns the difference of the two given sets,
+    * the set of all elements of `s` that are not in `t`.
+    */
+  def diff(s: Set, t: Set): Set = i => s(i) && !t(i)
+
+  /**
+    * Returns the subset of `s` for which `p` holds.
+    */
+  def filter(s: Set, p: Int => Boolean): Set = i => p(i) && s(i)
+
+  /**
+    * The bounds for `forall` and `exists` are +/- 1000.
+    */
+  val bound = 1000
+
+  /**
+    * Returns whether all bounded integers within `s` satisfy `p`.
+    */
+  def forall(s: Set, p: Int => Boolean): Boolean = {
+    var f = true
+    for(i <- -bound to bound) {
+      if(s(i) && !p(i)) f = false
+    }
+    f
+  }
+
+
+  /**
+    * Returns whether there exists a bounded integer within `s`
+    * that satisfies `p`.
+    */
+  def exists(s: Set, p: Int => Boolean): Boolean = {
+    var f = false
+    for(i <- -bound to bound) {
+      if(s(i) && p(i)) f = true
+    }
+    f
+  }
+
+  /**
+    * Returns a set transformed by applying `f` to each element of `s`.
+    */
+  def map(s: Set, f: Int => Int): Set = {
+    var r = diff(s, s)
+    for(i <- -bound to bound) {
+      if (s(i)) r = union(r, singletonSet(f(i)))
+    }
+    r
+  }
+
+  /**
+    * Displays the contents of a set
+    */
+  def toString(s: Set): String = {
+    val xs = for (i <- -bound to bound if contains(s, i)) yield i
+    xs.mkString("{", ",", "}")
+  }
+
+  /**
+    * Prints the contents of a set on the console.
+    */
+  def printSet(s: Set) {
+    println(toString(s))
+  }
+}
