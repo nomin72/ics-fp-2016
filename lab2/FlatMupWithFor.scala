b57785e16d91e9baa9cefad2946b3410f3b63913
Dzemite <nikol344265@rambler.ru>
2016-12-13 01:47:05 +0300

    change lab2, del no using functions and variables

--- lab2/FlatMupWithFor.scala
+++ lab2/FlatMupWithFor.scala
@@ -6,16 +6,11 @@ package flatMupWithFor
 
 class FlatMupWithFor[T] {
 
-  private var contents: T = _
-  def set(value: T) { contents = value }
-  def get: T = contents
-
   def flat_Mup(_list: List[List[T]], func: T => T): List[T] = {
 
     var out = _list.head.map(func)
-    val a = _list.length
 
-    for (i <- 1 until a)
+    for (i <- 1 until _list.length)
       out = out ::: _list(i).map(func)
 
     out
