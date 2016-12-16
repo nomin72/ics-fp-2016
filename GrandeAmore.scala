a7672918f35f9613230042fc89ee85f9f1b02470
Cindy <tuttobeneee@gmail.com>
2016-12-16 11:05:03 +0300

    without var

--- GrandeAmore.scala
+++ GrandeAmore.scala
@@ -1,8 +1,4 @@
-object GrandeAmore {
-  var nod = 0
-  def nod(m:Int, n:Int):Int= {
-    nod = if ( n == 0) m else nod(n, m % n)
-    nod
-  }
- }
+object GrandeAmore extends App{
+   def nod(m:Int, n:Int): Int = if ( n == 0) m else nod(n, m % n)
+}
 
